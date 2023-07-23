30d3b990f823c58d190f993977ad33d3f3ad057a
Pham Si Anh <phamsianh97@gmail.com>
2023-07-23 14:01:01 +0700

    fix listitem conflict

--- react-app/src/components/dashboard/listItems.js
+++ react-app/src/components/dashboard/listItems.js
@@ -10,50 +10,6 @@ import HomeRepairServiceIcon from '@mui/icons-material/HomeRepairService';
 import GroupsIcon from '@mui/icons-material/Groups';
 import GridViewIcon from '@mui/icons-material/GridView';
 
-<<<<<<< HEAD
-const listItems = [
-	{
-		to: '/',
-		primary: 'Trang Chủ',
-		icon: <DashboardIcon />,
-	},
-	{
-		to: '/bien-che',
-		primary: 'Biên Chế',
-		icon: <DashboardIcon />,
-	},
-	{
-		to: '/trang-bi',
-		primary: 'Trang Bị',
-		icon: <DashboardIcon />,
-	},
-	{
-		to: '/dieu-kien-huan-luyen',
-		primary: 'Điều Kiện Huấn Luyện',
-		icon: <DashboardIcon />,
-	},
-	{
-		to: '/noi-dung-bai-tap',
-		primary: 'Nội dung bài tập',
-		icon: <DashboardIcon />,
-	},
-];
-
-export const mainListItems = (
-	<React.Fragment>
-		{listItems.map((listItem) => (
-			<Link to={listItem.to} style={{ textDecoration: 'none' }}>
-				<ListItemButton>
-					<ListItemIcon>
-						{listItem.icon}
-					</ListItemIcon>
-					<ListItemText primary={listItem.primary} />
-				</ListItemButton>
-			</Link>
-		))}
-	</React.Fragment>
-);
-=======
 const listItems = [ 
  { 
   to: '/', 
@@ -101,4 +57,3 @@ export const mainListItems = (
   ))} 
  </React.Fragment> 
 );
->>>>>>> 32e3cf0722b03d2883c03482c8a0e8ebf989f1cf
